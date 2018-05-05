1b035436bc063301311197ff6af5fdf130621b98
Alex Johnson <alexrnau@gmail.com>
2018-05-04 21:24:00 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 'use strict';
 
-require.config({ paths: { 'vs': 'https://unpkg.com/monaco-editor@0.12.0/min/vs' } });
+require.config({ paths: { 'vs': 'https://unpkg.com/monaco-editor/min/vs' } });
 window.MonacoEnvironment = { getWorkerUrl: function getWorkerUrl() {
         return proxy;
     } };
-var proxy = URL.createObjectURL(new Blob(["\n\tself.MonacoEnvironment = {\n\t\tbaseUrl: 'https://unpkg.com/monaco-editor@0.12.0/min/'\n\t};\n\timportScripts('https://unpkg.com/monaco-editor@0.12.0/min/vs/base/worker/workerMain.js');\n"], { type: 'text/javascript' }));
+var proxy = URL.createObjectURL(new Blob(["\n\tself.MonacoEnvironment = {\n\t\tbaseUrl: 'https://unpkg.com/monaco-editor/min/'\n\t};\n\timportScripts('https://unpkg.com/monaco-editor/min/vs/base/worker/workerMain.js');\n"], { type: 'text/javascript' }));
 require(["vs/editor/editor.main"], function () {
     var editor = monaco.editor.create(document.getElementById('container'), {
         value: ['function x() {', '\tconsole.log("Hello world!");', '}'].join('\n'),
