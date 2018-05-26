4bb527a21c9798ed2d7b80c945acdb4592d5dcc7
Alex Johnson <alexrnau@gmail.com>
2018-05-25 20:42:40 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
-'use strict';
-
-require.config({ paths: { 'vs': 'https://unpkg.com/monaco-editor/min/vs' } });
-window.MonacoEnvironment = { getWorkerUrl: function getWorkerUrl() {
-        return proxy;
-    } };
-var proxy = URL.createObjectURL(new Blob(["\n\tself.MonacoEnvironment = {\n\t\tbaseUrl: 'https://unpkg.com/monaco-editor/min/'\n\t};\n\timportScripts('https://unpkg.com/monaco-editor/min/vs/base/worker/workerMain.js');\n"], { type: 'text/javascript' }));
-require(["vs/editor/editor.main"], function () {
-    var editor = monaco.editor.create(document.getElementById('container'), {
-        value: ['function x() {', '\tconsole.log("Hello world!");', '}'].join('\n'),
-        language: 'javascript',
-        theme: 'vs-dark'
-    });
-    editor.addListener('didType', function () {
-        console.log(editor.getValue());
-    });
+require.config({ paths: { vs: "https://unpkg.com/monaco-editor/min/vs" } });
+window.MonacoEnvironment = {
+  getWorkerUrl: function getWorkerUrl() {
+    return proxy;
+  }
+};
+var proxy = URL.createObjectURL(
+  new Blob(
+    [
+      "\n\tself.MonacoEnvironment = {\n\t\tbaseUrl: 'https://unpkg.com/monaco-editor/min/'\n\t};\n\timportScripts('https://unpkg.com/monaco-editor/min/vs/base/worker/workerMain.js');\n"
+    ],
+    { type: "text/javascript" }
+  )
+);
+require(["vs/editor/editor.main"], function() {
+  var editor = monaco.editor.create(document.getElementById("container"), {
+    value: [
+      "/**\n * Monaco code editor\n * Language: Javascript\n * Theme: vs-dark\n * \n * Write code here...\n */"
+    ].join("\n"),
+    language: "javascript",
+    theme: "vs-dark"
+  });
+  editor.addListener("didType", function() {
+    console.log(editor.getValue());
+  });
 });
