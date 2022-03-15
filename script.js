6eeb35ed04fe4d8f44d76daeda35dc8551bd4bae
Alex Johnson <22369976+alex-md@users.noreply.github.com>
2022-03-15 12:17:39 -0700

    style update

--- script.js
+++ script.js
@@ -1,12 +1,1 @@
-const parameters = {};
-const hashString = '#preset=react&=data' + encodeURIComponent(JSON.stringify(parameters));
-const iframe = document.createElement('iframe');
-iframe.src = '//unpkg.com/javascript-playgrounds/public/index.html' + hashString;
-document.body.appendChild(iframe);
-iframeStyle();
 
-function iframeStyle() {
-  iframe.style.width = '100vw';
-  iframe.style.height = '100vh';
-  iframe.style.border = '0';
-}
