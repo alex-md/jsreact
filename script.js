34eab81b245c8a5cafc476bd9f7f1239093bfc24
Alex Johnson <alexrnau@gmail.com>
2023-01-19 11:39:04 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const parameters = {};
 
 const hashString =
-	"#preset=javascript&=data" + encodeURIComponent(JSON.stringify(parameters));
+	"#preset=react&=data" + encodeURIComponent(JSON.stringify(parameters));
 const iframe = document.createElement("iframe");
 iframe.src =
 	"//unpkg.com/javascript-playgrounds/public/index.html" + hashString;
