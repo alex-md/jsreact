5b7ec107a753042c0a9846041622904f074132d1
Alex Johnson <alexrnau@gmail.com>
2023-05-22 10:29:16 -0700

    minor ui update

--- scripts/home.js
+++ scripts/home.js
@@ -1,9 +1,9 @@
 const parameters = {};
 
 const hashString =
-	"#preset=react&data=" + encodeURIComponent(JSON.stringify(parameters));
+	"#preset=javascript&data=" + encodeURIComponent(JSON.stringify(parameters));
 const iframe = document.createElement("iframe");
 iframe.src =
 	"//unpkg.com/javascript-playgrounds/public/index.html" + hashString;
 // place the iframe below the main
-document.querySelector("section").insertAdjacentElement("afterend", iframe);
+document.querySelector("main").insertAdjacentElement("afterend", iframe);
