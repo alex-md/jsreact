db1e1c3285bc3028d2807a366a2c0bdc968ab1c9
Alex Johnson <alexrnau@gmail.com>
2023-05-30 13:19:07 -0700

    Update

--- scripts/chat.js
+++ scripts/chat.js
@@ -71,30 +71,6 @@ submitButton.addEventListener("click", async () => {
 		});
 });
 
-
-// Wait for the DOM to load
-document.addEventListener('DOMContentLoaded', () => {
-	// Get all "navbar-burger" elements
-	const navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
-
-	// Check if there are any navbar burgers
-	if (navbarBurgers.length > 0) {
-		// Add a click event on each of them
-		navbarBurgers.forEach((navbarBurger) => {
-			navbarBurger.addEventListener('click', () => {
-				// Get the target from the "data-target" attribute
-				const target = navbarBurger.dataset.target;
-				const targetElement = document.getElementById(target);
-
-				// Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-				navbarBurger.classList.toggle('is-active');
-				targetElement.classList.toggle('is-active');
-			});
-		});
-	}
-});
-
-
 // Add event listener to input text element
 inputText.addEventListener("keydown", handleKeyDown);
 
