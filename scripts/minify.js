275d6b62d42bf6319e0d946820465adab2ba7842
Alex Johnson <alexrnau@gmail.com>
2023-06-13 19:27:28 -0700

    Incremental update

--- scripts/minify.js
+++ scripts/minify.js
@@ -1,8 +1,46 @@
-/**
- * Synchronizes scrolling between two elements.
- * @param {HTMLElement} inputElement - The element that triggers the scroll event.
- * @param {HTMLElement} outputElement - The element that should be scrolled.
- */
+import { minify } from "https://cdn.skypack.dev/terser@5.17.7";
+
+const minifyButton = document.getElementById("minify");
+const inputSizeLabel = document.getElementById("inputSizeLabel");
+const outputSizeLabel = document.getElementById("outputSizeLabel");
+const inputLabel = document.getElementById("inputLabel");
+const outputLabel = document.getElementById("outputLabel");
+const inputArea = document.getElementById("input");
+const outputArea = document.getElementById("output");
+const compressOption = document.getElementById("compress");
+const mangleOption = document.getElementById("mangle");
+const beautifyOption = document.getElementById("beautify");
+
+function updateSizeLabels() {
+    const inputText = inputArea.value;
+    const outputText = outputArea.value;
+    const inputSize = new Blob([inputText]).size;
+    const outputSize = new Blob([outputText]).size;
+    const inputKB = (inputSize / 1024).toFixed(2);
+    const outputKB = (outputSize / 1024).toFixed(2);
+    if (inputLabel) inputLabel.textContent = inputKB;
+    if (outputLabel) outputLabel.textContent = outputKB;
+}
+
+async function minifyCode() {
+    const inputText = inputArea.value;
+    const options = {
+        compress: compressOption.checked,
+        mangle: mangleOption.checked,
+        format: {
+            beautify: beautifyOption.checked
+        }
+    };
+    try {
+        const result = await minify(inputText, options);
+        if (result.error) throw result.error;
+        outputArea.value = result.code;
+        updateSizeLabels();
+    } catch (error) {
+        alert(error.message);
+    }
+}
+
 function synchronizeScroll(inputElement, outputElement) {
     function handleScroll(event) {
         const { scrollTop, scrollHeight, clientHeight } = event.target;
@@ -15,7 +53,11 @@ function synchronizeScroll(inputElement, outputElement) {
     inputElement.addEventListener("scroll", handleScroll);
     outputElement.addEventListener("scroll", handleScroll);
 }
+
+minifyButton.addEventListener("click", minifyCode);
+updateSizeLabels();
+
 const inputElement = document.querySelector("#input");
 const outputElement = document.querySelector("#output");
-synchronizeScroll(inputElement, outputElement);
 
+synchronizeScroll(inputElement, outputElement);
