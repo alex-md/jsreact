9517e8ef281d62c4f4b9aaf8e0d99d24978e8ff4
Alex Johnson <alexrnau@gmail.com>
2023-12-24 19:48:59 -0700

    small style changes

--- scripts/minify.js
+++ scripts/minify.js
@@ -1,20 +1,14 @@
-// Define an object that contains references to various DOM elements
 let DOM = {
-    go: document.getElementById("go"), // The "Compile" button
-    input: document.getElementById("input"), // The input code textarea
-    output: document.getElementById("output"), // The output code textarea
-    stats: document.getElementById("stats"), // The statistics textarea
-    log: document.getElementById("log"), // The log textarea
-    optionsWrapper: document.getElementById("options"), // The wrapper for the options
-    options: {} // An object that will contain the DOM elements for the options
+    go: document.getElementById("go"),
+    input: document.getElementById("input"),
+    output: document.getElementById("output"),
+    stats: document.getElementById("stats"),
+    log: document.getElementById("log"),
+    optionsWrapper: document.getElementById("options"),
+    options: {}
 };
-
-/**
- * Initializes the options object with the compilation level and formatting options.
- * Calls createOptions to create the user interface for the options.
- */
 function initializeOptions () {
-    let options = {
+    createOptions({
         compilation_level: {
             names: [
                 "Whitespace",
@@ -41,26 +35,13 @@ function initializeOptions () {
             default: "pretty_print",
             label: "Formatting"
         }
-    };
-    // Create the user interface for the options
-    createOptions(options, DOM.optionsWrapper, DOM.options);
+    }, DOM.optionsWrapper, DOM.options);
 }
-
-/**
- * Creates the options for the user interface based on the options object passed in.
- * @param {Object} options - The options object containing the compilation level and formatting options.
- * @param {HTMLElement} wrapper - The HTML element that will contain the options.
- * @param {Object} domOptions - The object that will contain the DOM elements for the options.
- */
 function createOptions (options, wrapper, domOptions) {
     let row;
     let keys = Object.keys(options);
     for (let i = 0; i < keys.length; i++) {
-        if (i % 2 === 0) {
-            row = document.createElement("div");
-            row.classList.add("row");
-            wrapper.appendChild(row);
-        }
+        i % 2 == 0 && ((row = document.createElement("div")).classList.add("row"), wrapper.appendChild(row));
         let col = document.createElement("div");
         col.classList.add("col-sm-6");
         let inputgroup = document.createElement("div");
@@ -68,33 +49,19 @@ function createOptions (options, wrapper, domOptions) {
         let inputgroupprepend = document.createElement("div");
         inputgroupprepend.classList.add("input-group-prepend");
         let title = document.createElement("span");
-        title.classList.add("input-group-text");
-        title.innerHTML = options[keys[i]].label;
+        title.classList.add("input-group-text"), title.innerHTML = options[keys[i]].label;
         let select = document.createElement("select");
-        select.classList.add("form-control");
-        domOptions[keys[i]] = select;
+        select.classList.add("form-control"), domOptions[keys[i]] = select;
         let selectoptions = options[keys[i]];
         for (let j = 0; j < selectoptions.names.length; j++) {
             let option = document.createElement("option");
-            option.value = selectoptions.values[j];
-            option.innerHTML = selectoptions.names[j];
-            select.appendChild(option);
+            option.value = selectoptions.values[j], option.innerHTML = selectoptions.names[j], select.appendChild(option);
         }
-        select.value = selectoptions.default;
-        inputgroupprepend.appendChild(title);
-        inputgroup.appendChild(inputgroupprepend);
-        inputgroup.appendChild(select);
-        col.appendChild(inputgroup);
-        row.appendChild(col);
+        select.value = selectoptions.default, inputgroupprepend.appendChild(title), inputgroup.appendChild(inputgroupprepend), inputgroup.appendChild(select), col.appendChild(inputgroup), row.appendChild(col);
     }
 }
 function compile () {
-    // Disable the "Compile" button and display "Compiling..." text
-    DOM.go.disabled = true;
-
-    DOM.go.innerHTML = "Compiling...";
-
-    // Build the URL for the Closure Compiler API
+    DOM.go.disabled = !0, DOM.go.innerHTML = "Compiling...";
     let url = buildURL('https://closure-compiler.appspot.com/compile', {
         js_code: DOM.input.value,
         compilation_level: DOM.options.compilation_level.value,
@@ -107,11 +74,7 @@ function compile () {
         ],
         formatting: DOM.options.formatting.value,
         language_out: 'ECMASCRIPT_2015'
-    });
-
-    // Callback function to handle the response from the API
-    let callback = function (data) {
-        // Parse the JSON response
+    }), callback = function (data) {
         let parsed;
         try {
             parsed = JSON.parse(data);
@@ -119,115 +82,48 @@ function compile () {
             showError("Invalid response from the API.");
             return;
         }
-
         let keys = Object.keys(parsed);
-
-        // Check if there are any compilation errors
         if (parsed.errors && parsed.errors.length > 0) {
-            showError("Compilation error: " + parsed.errors[0]['error'] + " on line " + parsed.errors[0]['lineno']);
+            showError("Compilation error: " + parsed.errors[0].error + " on line " + parsed.errors[0].lineno);
             return;
         }
-
-        // Iterate over the keys in the response object
-        for (let i = 0; i < keys.length; i++) {
-            switch (keys[i]) {
-                case 'compiledCode':
-                    // Display the compiled code in the output element
-                    // Trim the leading text 'use strict' and any whitespace
-                    DOM.output.innerHTML = parsed.compiledCode.replace(/^\s*'use strict';\s*/, '').trim();
-                    DOM.output.style.color = '#000'; // Set text color to black
-                    break;
-                case 'statistics':
-                    // Display statistics about the compression
-                    let s = parsed.statistics;
-                    DOM.stats.innerHTML = `Compressed ${s.originalSize} bytes down to ${s.compressedSize} bytes (${Math.round((s.originalSize - s.compressedSize) / s.originalSize * 100)}%)`;
-                    break;
-                default:
-                    // Log any other keys in the response object
-                    console.log(parsed[keys[i]]);
-            }
+        for (let i = 0; i < keys.length; i++)switch (keys[i]) {
+            case 'compiledCode':
+                DOM.output.innerHTML = parsed.compiledCode.replace(/^\s*'use strict';\s*/, '').trim(), DOM.output.style.color = '#000';
+                break;
+            case 'statistics':
+                let s = parsed.statistics;
+                DOM.stats.innerHTML = `Compressed ${s.originalSize} bytes down to ${s.compressedSize} bytes (${Math.round((s.originalSize - s.compressedSize) / s.originalSize * 100)}%)`;
+                break;
+            default:
+                console.log(parsed[keys[i]]);
         }
-
-        // Enable the "Go" button, restore its original text and appearance
-        DOM.go.disabled = false;
-        DOM.go.innerHTML = 'Compile!';
-        clearInterval(blinkingInterval);
-        DOM.go.classList.remove('blink');
-        DOM.go.classList.add('btn-success');
-    };
-
-    // Create a new XMLHttpRequest object
-    let xhttp = new XMLHttpRequest();
-
-    // Set up the callback function for handling the response
+        DOM.go.disabled = !1, DOM.go.innerHTML = 'Compile!', clearInterval(blinkingInterval), DOM.go.classList.remove('blink'), DOM.go.classList.add('btn-success');
+    }, xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function () {
-        if (this.readyState === 4) {
-            if (this.status === 200) {
-                callback.call(window, xhttp.responseText);
-            } else {
-                showError("An error occurred during the request.");
-            }
-        }
-    };
-
-    // Open a POST request to the URL
-    xhttp.open('POST', url);
-
-    // Specify that credentials should not be included in the request
-    xhttp.withCredentials = false;
-
-    // Send the request
-    xhttp.send();
+        4 === this.readyState && (200 === this.status ? callback.call(window, xhttp.responseText) : showError("An error occurred during the request."));
+    }, xhttp.open('POST', url), xhttp.withCredentials = !1, xhttp.send();
 }
-
 function showError (errorMessage) {
-    DOM.output.innerHTML = errorMessage;
-    DOM.output.style.color = '#b22b27'; // Set error text color to red
-    DOM.go.disabled = false;
-    DOM.go.innerHTML = 'Compile!';
-    clearInterval(blinkingInterval);
-    DOM.go.classList.remove('blink');
-    DOM.go.classList.add('btn-danger');
+    DOM.output.innerHTML = errorMessage, DOM.output.style.color = '#b22b27', DOM.go.disabled = !1, DOM.go.innerHTML = 'Compile!', clearInterval(blinkingInterval), DOM.go.classList.remove('blink'), DOM.go.classList.add('btn-danger');
 }
-
-
-// This function takes a domain and a params object and returns a URL string with the parameters appended to the domain.
 function buildURL (domain, params) {
-    // If the domain doesn't already have a query string, add one.
-    if (!domain.includes('?')) {
-        domain += '?';
-    }
-    // Loop through the keys of the params object.
+    domain.includes('?') || (domain += '?');
     let keys = Object.keys(params);
-    for (keys of keys) {
-        // If the value of the key is an array, loop through the array and add each value as a separate parameter.
-        if (Array.isArray(params[keys])) {
-            for (let i = 0; i < params[keys].length; i++) {
-                addParameter(params[keys][i]);
-            }
-        } else {
-            // If the value of the key is not an array, add it as a single parameter.
-            addParameter(params[keys]);
-        }
-    }
-    // Remove the trailing '&' character and return the URL string.
+    for (keys of keys) if (Array.isArray(params[keys])) for (let i = 0; i < params[keys].length; i++)addParameter(params[keys][i]);
+    else addParameter(params[keys]);
     return domain.slice(0, -1);
-    // This function adds a single parameter to the domain string.
     function addParameter (param) {
         domain += `${keys}=${encodeURIComponent(param)}&`;
     }
 }
-
 function blink () {
     DOM.go.classList.add('blink');
-    const blinkingInterval = setInterval(() => {
+    let blinkingInterval1 = setInterval(() => {
         DOM.go.classList.toggle('');
     }, 500);
     setTimeout(() => {
-        clearInterval(blinkingInterval);
-    }, 5000);
-    compile();
+        clearInterval(blinkingInterval1);
+    }, 5000), compile();
 }
-
-DOM.go.onclick = blink;
-initializeOptions();
+DOM.go.onclick = blink, initializeOptions();
